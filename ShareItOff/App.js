0a58e791f32acb905fba8971c99b7a91a1191cf8
Juliette Sabatier <jsabatier001@ensc.fr>
2023-03-07 16:06:17 +0100

    RequeteAPIOK

--- ShareItOff/App.js
+++ ShareItOff/App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { Text, View, ActivityIndicator, TouchableOpacity } from "react-native";
+import styles from "./theme/styles";
+import fetchArtist from "./api/lastfmApi";
 
-export default function App() {
+export default App = () => {
+  // Define state
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [artistName, setArtistName] = useState("");
+  const [artsistDescription, setArtistDescription] = useState("");
+
+  const loadArtist = async () => {
+    setLoading(true);
+    setError(false);
+
+    try {
+      const artist = await fetchArtist();
+      setArtistName(artist.name);
+      setArtistDescription(artist.bio.summary);
+    } catch (e) {
+      setError(true);
+    }
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    loadArtist();
+  }, []);
+
+  function load(loading) {
+    if (loading) {
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator size="large" />
+        </View>
+      );
+    }
+  }
+
+  load({ loading });
+
+  if (error) {
+    return (
+      <View style={[styles.container, { alignItems: "center" }]}>
+        <Text>Something went wrong :\</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.name}>
+        {artistName}
+        {artsistDescription}
+      </Text>
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
